188448f775e463ed80ba8b036a321c03cc03f653
Giorgos Tzampanakis <g_tzampanakis@wargaming.net>
2024-07-11 10:26:16 +0300

    WIP

--- src/main.c
+++ src/main.c
@@ -3,30 +3,32 @@
 
 #define POSITION_MEMORY_SIZE (1024 * 1024)
 
-#define COLOR_WHITE 0b0000
-#define COLOR_BLACK 0b1000
+#define COLOR_WHITE 0b00000
+#define COLOR_BLACK 0b11000
+#define COLOR_EMPTY 0b10000
 
-#define PIECE_EMPTY     0b000
-#define PIECE_PAWN      0b001
-#define PIECE_ROOK      0b010
-#define PIECE_KNIGHT    0b011
-#define PIECE_BISHOP    0b100
-#define PIECE_QUEEN     0b101
-#define PIECE_KING      0b110
+#define UNCOLORED_PAWN      0b001
+#define UNCOLORED_ROOK      0b010
+#define UNCOLORED_KNIGHT    0b011
+#define UNCOLORED_BISHOP    0b100
+#define UNCOLORED_QUEEN     0b101
+#define UNCOLORED_KING      0b110
 
-#define P_WHITE ( COLOR_WHITE | PIECE_PAWN )
-#define R_WHITE ( COLOR_WHITE | PIECE_ROOK )
-#define N_WHITE ( COLOR_WHITE | PIECE_KNIGHT )
-#define B_WHITE ( COLOR_WHITE | PIECE_BISHOP )
-#define Q_WHITE ( COLOR_WHITE | PIECE_QUEEN )
-#define K_WHITE ( COLOR_WHITE | PIECE_KING )
+#define PIECE_EMPTY     ( COLOR_EMPTY | 0b000 )
+
+#define P_WHITE ( COLOR_WHITE | UNCOLORED_PAWN )
+#define R_WHITE ( COLOR_WHITE | UNCOLORED_ROOK )
+#define N_WHITE ( COLOR_WHITE | UNCOLORED_KNIGHT )
+#define B_WHITE ( COLOR_WHITE | UNCOLORED_BISHOP )
+#define Q_WHITE ( COLOR_WHITE | UNCOLORED_QUEEN )
+#define K_WHITE ( COLOR_WHITE | UNCOLORED_KING )
  
-#define P_BLACK ( COLOR_BLACK | PIECE_PAWN )
-#define R_BLACK ( COLOR_BLACK | PIECE_ROOK )
-#define N_BLACK ( COLOR_BLACK | PIECE_KNIGHT )
-#define B_BLACK ( COLOR_BLACK | PIECE_BISHOP )
-#define Q_BLACK ( COLOR_BLACK | PIECE_QUEEN )
-#define K_BLACK ( COLOR_BLACK | PIECE_KING )
+#define P_BLACK ( COLOR_BLACK | UNCOLORED_PAWN )
+#define R_BLACK ( COLOR_BLACK | UNCOLORED_ROOK )
+#define N_BLACK ( COLOR_BLACK | UNCOLORED_KNIGHT )
+#define B_BLACK ( COLOR_BLACK | UNCOLORED_BISHOP )
+#define Q_BLACK ( COLOR_BLACK | UNCOLORED_QUEEN )
+#define K_BLACK ( COLOR_BLACK | UNCOLORED_KING )
 
 #define CASTLING_BLACK_KINGSIDE     0b1000;
 #define CASTLING_BLACK_QUEENSIDE    0b0100;
@@ -34,6 +36,9 @@
 #define CASTLING_WHITE_QUEENSIDE    0b0001;
 
 #define MOVE_LIST_INITIAL_CAPACITY 64
+
+#define N_FILES 8
+#define N_RANKS 8
 
 typedef char Piece;
 typedef char Castling;
@@ -118,13 +123,15 @@ void truncate_move_list(MoveList *ml) {
 }
 
 typedef struct Pos {
-    Piece placement[8][8];
+    Piece placement[N_FILES][N_RANKS];
     Color active_color;
     Castling castling;
     Sq en_passant;
     int halfmoves;
     int fullmoves;
     int is_king_in_check;
+    int is_king_in_checkmate;
+    int is_king_in_stalemate;
 
     MoveList moves;
 } Pos;
@@ -135,8 +142,8 @@ Pos *make_position() {
          printf("Unable to allocation memory for position. Aborting...");
          abort();
      }
-     for (int i = 0; i < 8; i++) {
-         for (int j = 0; j < 8; j++) {
+     for (int i = 0; i < N_FILES; i++) {
+         for (int j = 0; j < N_RANKS; j++) {
              p->placement[i][j] = PIECE_EMPTY;
          }
      }
@@ -146,6 +153,8 @@ Pos *make_position() {
      p->halfmoves = 0;
      p->fullmoves = 0;
      p->is_king_in_check = -1;
+     p->is_king_in_checkmate = -1;
+     p->is_king_in_stalemate = -1;
      init_move_list(&p->moves);
      return p;
 }
@@ -296,11 +305,11 @@ Pos *decode_fen(char *fen_string) {
 }
 
 Color piece_color(Piece piece) {
-    return 0b1000 & piece;
+    return 0b11000 & piece;
 }
 
 Piece piece_as_white(Piece piece) {
-    return 0b0111 & piece;
+    return 0b00111 & piece;
 }
 
 typedef void (*ApplyDirFn)(Sq*);
@@ -343,7 +352,7 @@ ApplyDirFn white_pawn_capture_dir_fns[] = {
 ApplyDirFn black_pawn_capture_dir_fns[] = {
                 apply_dir_dr, apply_dir_dl, NULL };
 
-MoveList legal_moves_for_piece(Pos* pos, Sq sq0, Piece piece, MoveList *ml) {
+void append_legal_moves_for_piece(Pos* pos, Sq sq0, Piece piece, MoveList *ml) {
     Color own_color = piece_color(piece);
 
     ApplyDirFn *dir_fns;
@@ -528,9 +537,9 @@ int is_king_in_square_in_check(Pos *pos, Sq sq0) {
 
 int is_king_in_check(Pos *pos) {
     Color active_color = pos->active_color;
-    Piece king_to_find = active_color | PIECE_KING;
-    for (int f = 0; f < 8; f++) {
-        for (int r = 0; r < 8; r++) {
+    Piece king_to_find = active_color | UNCOLORED_KING;
+    for (int f = 0; f < N_FILES; f++) {
+        for (int r = 0; r < N_RANKS; r++) {
             Sq sq = make_sq(f, r);
             Piece found = get_piece_at_sq(pos, sq);
             if (found == king_to_find) {
@@ -541,8 +550,42 @@ int is_king_in_check(Pos *pos) {
     return 0;
 }
 
-void expand_position(Pos *pos) {
+void set_legal_moves_for_position(Pos *pos) {
+    Color active_color = pos->active_color;
+    for (int f = 0; f < N_FILES; f++) {
+        for (int r = 0; r < N_RANKS; r++) {
+            Sq sq = make_sq(f, r);
+            Piece found = get_piece_at_sq(pos, sq);
+            Color found_color = piece_color(found);
+            if (active_color == found_color) {
+                //print_sq(sq);
+                append_legal_moves_for_piece(pos, sq, found, &pos->moves);
+            }
+        }
+    }
+}
+
+int is_king_in_checkmate(Pos *pos) {
+    return pos->is_king_in_check & (pos->moves.len == 0);
+}
+
+int is_king_in_stalemate(Pos *pos) {
+    return (!pos->is_king_in_check) & (pos->moves.len == 0);
+}
+
+void set_is_king_in_checkmate(Pos *pos) {
+    pos->is_king_in_checkmate = is_king_in_checkmate(pos);
+}
+
+void set_is_king_in_stalemate(Pos *pos) {
+    pos->is_king_in_stalemate = is_king_in_stalemate(pos);
+}
+
+void explore_position(Pos *pos) {
     pos->is_king_in_check = is_king_in_check(pos);
+    set_legal_moves_for_position(pos);
+    set_is_king_in_checkmate(pos);
+    set_is_king_in_stalemate(pos);
 }
 
 int main() {
@@ -553,19 +596,25 @@ int main() {
                 "8/4k3/3N1N2/4Q3/1B6/8/1K6/8 b - - 0 1";
     char fen[] = "8/4k3/3P1P2/4Q3/1B6/8/1K6/8 w - - 0 1";
 
-    Pos *pos = decode_fen("6Nr/2K1p3/5B2/8/8/1k1PnP2/2P1P3/8 b - - 0 1");
+    Pos *pos = decode_fen(starting_fen);
+    explore_position(pos);
 
-    MoveList ml;
-    init_move_list(&ml);
-    Sq sq = make_sq(4, 6);
-    legal_moves_for_piece(pos, sq, P_BLACK, &ml);
-    for (int i = 0; i < ml.len; i++) {
-        Move move2 = *(ml.data + i);
-        print_sq(move2.to);
-    }
-    printf("%d\n", is_king_in_check(pos));
-
-    free_move_list(&ml);
+    //MoveList ml;
+    //init_move_list(&ml);
+    //Sq sq = make_sq(4, 6);
+    //append_legal_moves_for_piece(pos, sq, P_BLACK, &ml);
+    //for (int i = 0; i < ml.len; i++) {
+    //    Move move2 = *(ml.data + i);
+    //    print_sq(move2.to);
+    //}
+    //printf("%d\n", is_king_in_check(pos));
+    //free_move_list(&ml);
+    
+    //for (int i = 0; i < pos->moves.len; i++) {
+    //    Move move = pos->moves.data[i];
+    //    print_sq(move.from);
+    //    print_sq(move.to);
+    //}
 
     free_position(pos);
 
